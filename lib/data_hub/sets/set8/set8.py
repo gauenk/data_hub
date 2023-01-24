62fda38e548a1231a1f9316b322ba46df2e41f36
gauenk <kent.gauen@gmail.com>
2023-01-24 16:07:32 -0500

    cleanup set8; stale

--- lib/data_hub/sets/set8/set8.py
+++ lib/data_hub/sets/set8/set8.py
@@ -143,68 +143,6 @@ def load(cfg):
               "cropmode":"center"}
     p = parse_cfg(cfg,modes,fields)
 
-
-    # # -- noise and dyanmics --
-    # noise_info = noise_from_cfg(cfg)
-
-    # # -- set-up --
-    # modes = ['tr','val','te']
-
-    # # -- bw --
-    # def_bw = optional(cfg,"bw",False)
-    # bw = edict()
-    # for mode in modes:
-    #     bw[mode] = optional(cfg,"bw_%s"%mode,def_bw)
-
-    # # -- frames --
-    # def_nframes = optional(cfg,"nframes",0)
-    # nframes = edict()
-    # for mode in modes:
-    #     nframes[mode] = optional(cfg,"nframes_%s"%mode,def_nframes)
-
-    # # -- fstride [amount of overlap for subbursts] --
-    # def_fstride = optional(cfg,"fstride",1)
-    # fstride = edict()
-    # for mode in modes:
-    #     fstride[mode] = optional(cfg,"%s_fstride"%mode,def_fstride)
-
-    # # -- frame sizes --
-    # def_isize = optional(cfg,"isize",None)
-    # if def_isize == "-1_-1": def_size = None
-    # isizes = edict()
-    # for mode in modes:
-    #     isizes[mode] = get_isize(optional(cfg,"isize_%s"%mode,def_isize))
-
-    # # -- samples --
-    # def_nsamples = optional(cfg,"nsamples",-1)
-    # nsamples = edict()
-    # for mode in modes:
-    #     nsamples[mode] = optional(cfg,"nsamples_%s"%mode,def_nsamples)
-
-    # # -- crop mode --
-    # def_cropmode = optional(cfg,"cropmode","region_center")
-    # cropmode = edict()
-    # for mode in modes:
-    #     cropmode[mode] = optional(cfg,"cropmode_%s"%mode,def_cropmode)
-
-    # # -- random order --
-    # def_rand_order = optional(cfg,'rand_order',False)
-    # rand_order = edict()
-    # for mode in modes:
-    #     rand_order[mode] = optional(cfg,"rand_order_%s"%mode,def_rand_order)
-
-    # # -- random order --
-    # def_index_skip = optional(cfg,'index_skip',1)
-    # index_skip = edict()
-    # for mode in modes:
-    #     index_skip[mode] = optional(cfg,"index_skip_%s"%mode,def_index_skip)
-
-    # # -- batch size --
-    # def_batch_size = optional(cfg,'batch_size',1)
-    # batch_size = edict()
-    # for mode in modes:
-    #     batch_size[mode] = optional(cfg,'batch_size_%s'%mode,def_batch_size)
-
     # -- setup paths --
     iroot = IMAGE_PATH
     sroot = IMAGE_SETS
