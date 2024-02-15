49331f280ad419ecf1d1ea0b1b60d156788e8f45
gauenk <kent.gauen@gmail.com>
2024-02-15 06:04:33 -0500

    update davis

--- lib/data_hub/sets/davis/paths.py
+++ lib/data_hub/sets/davis/paths.py
@@ -4,10 +4,12 @@ import os
 from pathlib import Path
 pcname = os.uname().nodename
 if "anvil" in pcname:
-    base = Path("/home/x-kgauen/")
+    base = Path("/home/x-kgauen/Documents/")
+elif "USING_DOCKER" in os.environ:
+    base = Path("/working/")
 else:
-    base = Path("/home/gauenk/")
-BASE = base / Path("Documents/data/davis/DAVIS/")
+    base = Path("/home/gauenk/Documents/")
+BASE = base / Path("data/davis/DAVIS/")
 IMAGE_PATH = BASE / Path("./JPEGImages/480p/")
 IMAGE_SETS = BASE / Path("./ImageSets/2017/")
 CROPPED_BASE = BASE / "./cropped"
