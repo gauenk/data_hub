15b50b9080306108da5967a049441f9c4c23bd57
gauenk <kent.gauen@gmail.com>
2024-02-15 06:07:51 -0500

    update paths

--- lib/data_hub/sets/set8/paths.py
+++ lib/data_hub/sets/set8/paths.py
@@ -1,6 +1,13 @@
-
+import os
 from pathlib import Path
-BASE = Path("/home/gauenk/Documents/data/set8/")
+pcname = os.uname().nodename
+if "anvil" in pcname:
+    base = Path("/home/x-kgauen/Documents/")
+elif "USING_DOCKER" in os.environ:
+    base = Path("/working/")
+else:
+    base = Path("/home/gauenk/Documents/")
+BASE = base / Path("data/set8/")
 IMAGE_PATH = BASE / Path("./images/")
 IMAGE_SETS = BASE / Path("./image_sets/")
 FLOW_BASE =  BASE / Path("./flows/")
